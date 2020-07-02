eb33f08b5c54bad0ff23323c5cae680f88cc200d
Colby Witherup Wood <colby.witherup@northwestern.edu>
2020-07-01 22:49:58 -0500

    initial commit

---
+++ acre_script.py
@@ -0,0 +1,36 @@
+#This is a comment. The hashtag at the beginning of the line tells
+#the computer to ignore these lines. You must put a new hashtag
+#for each line.
+
+"""
+For longer comments, put them inside triple quotes.
+Everything inside the triple quotes will be ignored.
+"""
+
+"""
+This is a script that can be run from the command line.
+This file does not need to be opened to run the script.
+Run the script on the command line by navigating to the
+folder that contains this script and
+typing python acre_script.py.
+"""
+
+#if you have any modules to import, import them at the top of the script
+
+#assign any variables here
+test_list = [5000, 6500, 8000, 15251]
+factor = 43560
+
+#include any function definitions here
+def sqft_to_acres(sqft):
+	#this function takes square footage and returns rounded acreage
+	acres = sqft/factor
+	return round(acres, 2)
+
+#the rest of your code, including calling any functions, goes here
+for test_value in test_list:
+	answer = sqft_to_acres(test_value)
+	print(str(test_value) + " square feet is " + str(answer) + " acres.")
+
+
+        
